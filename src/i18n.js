35680d020a966d1ad8c466120c27dac7c5b8b075
Jennifer Jang <85824661+JCJang@users.noreply.github.com>
2021-12-13 09:41:10 -0500

    dec 13 removed language detection from i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,5 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 import translationZHTW from './translations/translationZHTW.json'
 import translationZHCN from './translations/translationZHCN.json'
@@ -8,10 +7,6 @@ import translationEN from './translations/translationEN.json'
 
 
 i18n
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
