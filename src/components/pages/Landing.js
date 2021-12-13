1eda9efac32343d2000ea88beb235b7a12979f45
Jennifer Jang <85824661+JCJang@users.noreply.github.com>
2021-12-13 10:49:03 -0500

    dec 13 removing preload greatly reduced load times. Added webp landing images for further improvement.

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -2,6 +2,9 @@ import ImageFadeIn from './../util/ImageFadeIn'
 import landingLg from '../../photos/landing-lg.jpg'
 import landingMd from '../../photos/landing-md.jpg'
 import landing from '../../photos/landing.jpg'
+import landingLgWebP from '../../photos/landing-lg.webp'
+import landingMdWebP from '../../photos/landing-md.webp'
+import landingWebP from '../../photos/landing.webp'
 import {Helmet} from "react-helmet";
 import { useTranslation } from 'react-i18next'
 
@@ -36,9 +39,9 @@ const Landing = () => {
       <div className="relative shadow-md justify-center h-screen w-screen">
           <div className="absolute h-screen w-screen gradient darken-layer"></div>
               <picture>
-          <source className="h-screen w-screen object-cover" media="(min-width:1024px)" srcset={landingLg}></source>
-          <source className="h-screen w-screen object-cover" media="(min-width:768px)" srcset={landingMd}></source>
-          <ImageFadeIn className="h-screen w-screen object-cover" src={landing} alt="Scenic View of Taroko Gorge"/>
+          <source className="h-screen w-screen object-cover" media="(min-width:1024px)" srcset={landingLgWebP, landingLg}></source>
+          <source className="h-screen w-screen object-cover" media="(min-width:768px)" srcset={landingMdWebP, landingMd}></source>
+          <ImageFadeIn className="h-screen w-screen object-cover" src={landingWebP, landing} alt="Scenic View of Taroko Gorge"/>
           </picture>
           <div className="absolute top-0 w-screen h-screen flex flex-col justify-center items-center">
             {t("Navbar.language")!=="en"?
